a3467359c77e2d6d97577bbf7c71cc65d9e22903
[Richard Mark Haes-Ellis] <[richaeell@alum.us.es]>
2019-02-27 13:13:03 +0100

    Test·

--- balancin.m
+++ balancin.m
@@ -1,5 +1,5 @@
 clear all
-clc
+clc;
 
 K = [0 0 0];
 Kfric = 1.5;
@@ -20,7 +20,7 @@ Id = (1/2)*md*(Re^2+Ri^2); % Hollow Cylinder
 t0 = 0;
 tn = 10;
 
-% State space 
+% State space
 A = [-((ms*g*l)/Kfric)  0          -(Is/Kfric);
      ((ms*g*l)/Is)       -Kfric/Is         0;
        0                ((ms*g*l)/Is)   -(Kfric/Is)];
@@ -44,13 +44,13 @@ eig(A-B*K)
 
 % -------------------------------------------------------------------
 
-%Run the model 
+%Run the model
 sim('model2',[t0 tn]);
 
 
 %-----------Static Plotting----------------
 
-%Plot angular displacement of mass 
+%Plot angular displacement of mass
 figure('rend','painters','pos',[100 100 1200 500])
 
 
@@ -58,25 +58,25 @@ figure('rend','painters','pos',[100 100 1200 500])
         subplot(3,4,[1 2])
         plot(t,Pos)
         title('Angular Displacement of Mass                                ')
-        xlabel('time (s)'); 
+        xlabel('time (s)');
         ylabel('\theta (rad)');
         grid;
         subplot(3,4,[5 6])
         plot(t,MAcceleration)
-        title('Motor Acceleration                                          ') 
-        xlabel('time (s)'); 
+        title('Motor Acceleration                                          ')
+        xlabel('time (s)');
         ylabel('\omega (rad/s^2)');
         grid;
         subplot(3,4,[9 10])
         plot(t,MTorque)
         title('Motor Torque                                                ')
-        xlabel('time (s)'); 
+        xlabel('time (s)');
         ylabel('\tau (Nm)');
         grid;
 
 %--------------Dynamic Plotting--------------
 
-% Create the markers representing the pendulum which will 
+% Create the markers representing the pendulum which will
 % Vary according to calculated positions
 subplot(3,4,[3 4 7 8 11 12])
 h=plot(0,0,'MarkerSize',10,'Marker','+','LineWidth',5); %the BC
@@ -90,16 +90,16 @@ hold off
 % Animation space
 range=1.5*l; axis([-range range -range range]); axis square;
 title('Pendulum Animation')
-xlabel('x displacement'); 
+xlabel('x displacement');
 ylabel('y displacement');
 grid;
 set(gca,'nextplot','replacechildren');  %line to fresh each plot
 
 for i=1:length(Pos)-1
     if (ishandle(h1)==1) % check figure is plotting
-        % x cordinate of mass 
+        % x cordinate of mass
         Xcoord=[0,l*sin(Pos(i))];
-        % y cordinate of mass 
+        % y cordinate of mass
         Ycoord=[0,l*cos(Pos(i))];
         % Update x and y cords
         set(h2,'XData',Xcoord(2),'YData',Ycoord(2));
@@ -107,7 +107,3 @@ for i=1:length(Pos)-1
         drawnow;
     end
 end
-
-
-
-
