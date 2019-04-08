4f1cd3173ca5463777d60937dfe0a8d1b0fa6b0e
[Richard Mark Haes-Ellis] <[richaeell@alum.us.es]>
2019-04-09 00:36:29 +0200

    Funcionando UDP basico

--- esp8266/main/main.ino
+++ esp8266/main/main.ino
@@ -1,50 +1,63 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 
-const char* ssid = "Selba";
-const char* password = "Selba123";
+#ifndef STASSID
+#define STASSID "Selba"
+#define STAPSK  "Selba123"
+#endif
 
 IPAddress local_IP(192,168,4,20);
 IPAddress gateway(192,168,4,1);
 IPAddress subnet(255,255,255,0);
 
-IPAddress broadcastIp;
+unsigned int localPort = 8888;      // local port to listen on
 
-WiFiUDP udp;
-unsigned int localUdpPort = 4200;
-char replyPacket[] = "Hi there! Got the message :-)";
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
+char  ReplyBuffer[] = "acknowledged\r\n";       // a string to send back
 
+WiFiUDP Udp;
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
-  Serial.println();
-
-
-  while(!Serial){}
-
-  Serial.println("Starting...");
-
-  delay(1000);
 
   Serial.print("Setting soft-AP ... ");
   Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
-  Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
+  Serial.println(WiFi.softAP(STASSID,STAPSK) ? "Ready" : "Failed!");
 
-  Serial.println("Starting UDP ... ");
-  udp.begin(localUdpPort);
-  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), localUdpPort);
-
-  // set broadcast and gateway ip
-  IPAddress ipBroadcast(192,168,4,255);
+  Serial.printf("UDP server on port %d\n", localPort);
+  Udp.begin(localPort);
 }
 
 void loop() {
-  // broadcast udp package
-  udp.beginPacket(ipBroadcast, 5005);
-  udp.write("{ \"sensor\" : \"esp8266\", \"value\": \"hello from esp8266\" }");
-  udp.endPacket();
-  // wait 1  seconds
-  delay(1000);
-
+  // if there's data available, read a packet
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {
+    Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    Serial.print("From ");
+    IPAddress remote = Udp.remoteIP();
+    for (int i = 0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+        if (i < 3) {
+        Serial.print(".");
+      }
+    }
+    Serial.print(", port ");
+    Serial.println(Udp.remotePort());
+      // read the packet into packetBufffer
+    Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+    Serial.println("Contents:");
+    Serial.println(packetBuffer);
+      // send a reply, to the IP address and port that sent us the packet we received
+      Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+    Udp.write(ReplyBuffer);
+    Udp.endPacket();
+  }
+  delay(10);
 }
+  /*
+    test (shell/netcat):
+    --------------------
+  	  nc -u 192.168.esp.address 8888
+  */
