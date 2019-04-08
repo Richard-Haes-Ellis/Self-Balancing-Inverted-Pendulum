e87668b0cdac69d7f15b0554b54a5543511493e1
[Richard Mark Haes-Ellis] <[richaeell@alum.us.es]>
2019-04-09 00:28:18 +0200

    conexion udp

--- esp8266/main/main.ino
+++ esp8266/main/main.ino
@@ -1,4 +1,19 @@
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+const char* ssid = "Selba";
+const char* password = "Selba123";
+
+IPAddress local_IP(192,168,4,20);
+IPAddress gateway(192,168,4,1);
+IPAddress subnet(255,255,255,0);
+
+IPAddress broadcastIp;
+
+WiFiUDP udp;
+unsigned int localUdpPort = 4200;
+char replyPacket[] = "Hi there! Got the message :-)";
+
 
 void setup()
 {
@@ -10,18 +25,26 @@ void setup()
 
   Serial.println("Starting...");
 
-  WiFi.begin("MiFibra-ADCD", "5DhSTp6U");
+  delay(1000);
 
-  Serial.print("Connecting");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println();
+  Serial.print("Setting soft-AP ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet) ? "Ready" : "Failed!");
+  Serial.println(WiFi.softAP(ssid,password) ? "Ready" : "Failed!");
 
-  Serial.print("Connected, IP address: ");
-  Serial.println(WiFi.localIP());
+  Serial.println("Starting UDP ... ");
+  udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.softAPIP().toString().c_str(), localUdpPort);
+
+  // set broadcast and gateway ip
+  IPAddress ipBroadcast(192,168,4,255);
 }
 
-void loop() {}
+void loop() {
+  // broadcast udp package
+  udp.beginPacket(ipBroadcast, 5005);
+  udp.write("{ \"sensor\" : \"esp8266\", \"value\": \"hello from esp8266\" }");
+  udp.endPacket();
+  // wait 1  seconds
+  delay(1000);
+
+}
