bbc9805e7e534825e7150843715f0978230e83bf
richaeell <richard9661h@gmail.com>
2019-04-16 00:45:08 +0200

    Añadido algunas funciones para mandar datos a matlab

---
+++ esp8266/main/build/core/core_esp8266_noniso.c.d
@@ -0,0 +1,24 @@
+/home/pirana/Repos/Self-Balancing-Inverted-Pendulum/esp8266/build/core/core_esp8266_noniso.c.o: \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/cores/esp8266/core_esp8266_noniso.c \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/stdlib.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/machine/ieeefp.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/newlib.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/config.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/reent.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/_types.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/machine/_types.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/lock.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/cdefs.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/machine/stdlib.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/alloca.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/string.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/string.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/tools/sdk/libc/xtensa-lx106-elf/include/math.h \
+ /home/pirana/.arduino15/packages/esp8266/hardware/esp8266/2.5.0/cores/esp8266/stdlib_noniso.h
--- esp8266/main/main.ino
+++ esp8266/main/main.ino
@@ -5,7 +5,11 @@
 #define STASSID "Selba"
 #define STAPSK  "Selba123"
 
+#define doubleLength 8 
+#define tickToRad 0.006135923
 
+#define serialTelemetry
+ 
 
 IPAddress local_IP(192,168,4,20);
 IPAddress gateway(192,168,4,1);
@@ -18,38 +22,44 @@ char  ReplyBuffer[100];  // Buffers forsending data
 WiFiUDP Udp;
 
 
-uint8_t encoderPin_x = 0; // D3 == 0
-uint32_t tOn_x = 0;
-uint32_t period_x = 0;
-uint32_t startTime_x = 0;
+uint8_t x_ChannelAPin = 5; 
+uint8_t x_ChannelBPin = 4; 
+int32_t xEnconderTick = 0;
+float x_theta = 0;
+double x_theta_dot = 0;
+float x_theta_prv = 0;
+
 uint32_t deltaT = 0;
-uint32_t prv_Time = 0;
+uint32_t timer0 = 0;
+uint32_t timer1 = 0;
+
 float phi_x = 0;
 float phi_x_dot = 0;
 float prv_phi_x = 0;
-float k = 0;
+float ratio_x = 0;
+
+byte buffer4[4];
 
 
 
-void encoder_x_Handler(){
-  uint8_t currState = 0;
-  if(digitalRead(encoderPin_x)){
-    period_x = micros() - startTime_x;
-    startTime_x = micros();
-  }else{
-    tOn_x = micros() - startTime_x;
-  }
-}
 
 void setup() {
+  delay(5000);
   Serial.begin(115200);
+  #ifdef debug
+    Serial.println("\nStarting...");
+  #endif
+  delay(2000);
 
-  delay(5);
+  #ifdef debug
+    Serial.println("Configuring system.. ");
+  #endif
+  pinMode(x_ChannelAPin,INPUT);
+  pinMode(x_ChannelBPin,INPUT);
+  attachInterrupt(digitalPinToInterrupt(x_ChannelAPin),x_channelAInterruptHandler,CHANGE);
+  attachInterrupt(digitalPinToInterrupt(x_ChannelBPin),x_channelBInterruptHandler,CHANGE);
 
-  pinMode(encoderPin_x,INPUT);
-  attachInterrupt(digitalPinToInterrupt(encoderPin_x),encoder_x_Handler,CHANGE);
-
-
+    
   // NETWORK AND UDP CONFIG // 
   // WiFi.softAPConfig(local_IP, gateway, subnet);
   // Serial.println(WiFi.softAP(STASSID,STAPSK) ?  "Soft-AP Ready" : "soft-AP Failed!");
@@ -57,36 +67,141 @@ void setup() {
   // Serial.printf("Wifi: %s \nPassword: %s\n",STASSID,STAPSK);
   // Serial.print("Broadcasting on port: ");
   // Serial.println(broadcastPort);
-
+  #ifdef debug
+    Serial.println("Setup done!");
+  #endif
 }
 
 void loop() {
-  if(period_x!=0){
-    phi_x =2*PI*k + 2*PI*((float)tOn_x/(float)period_x);
+
+  x_theta = xEnconderTick*tickToRad;
+
+  if(micros()-timer1>100)
+  {
+    timer1 = micros();
+    x_theta_dot = (x_theta_prv-x_theta)/0.0001;
+    x_theta_prv = x_theta;
+    #ifdef serialTelemetry
+      printDouble(x_theta_dot,100000000);
+    #endif
   }
 
   
-  deltaT = micros()-prv_Time;
-  if(deltaT>0){
-    phi_x_dot = (prv_phi_x - phi_x)/deltaT;
+  if(micros()-timer0>10000) // Every 10000 microseconds OR 0.01 milliseconds
+  {
+    #ifdef matlab
+      // MatlabSendInt(xEnconderTick);
+      // MatlabSendDouble((double)PI);
+    #endif
+    timer0 = micros();
   }
-  prv_Time = micros();
-  prv_phi_x = phi_x;
-
-  Serial.print(phi_x);
-  Serial.print(" ");
-  Serial.printf("%f \n\r",phi_x_dot);
-
 
 
   // Broadcast data
   // Udp.beginPacket(broadcastIp,broadcastPort);
   // Udp.write(ReplyBuffer);
   // Udp.endPacket();
-
-}
   /*
     test (shell/netcat):
     --------------------
   	  nc -kluvw 0  *PORT*
   */
+
+}
+
+// Low pass bessel filter order=1 alpha1=0.01 
+// http://www.schwietering.com/jayduino/filtuino/
+class  FilterBeLp1
+{
+	public:
+		FilterBeLp1()
+		{
+			v[0]=0.0;
+		}
+	private:
+		double v[2];
+	public:
+		double step(double x) //class II 
+		{
+			v[0] = v[1];
+			v[1] = (3.046874709125380054e-2 * x) + (0.93906250581749239892 * v[0]);
+			return (v[0] + v[1]);
+		}
+};
+
+
+void x_channelAInterruptHandler()
+{
+  if(digitalRead(x_ChannelAPin)){     // If A is rising
+    if(!digitalRead(x_ChannelBPin)){  // and B is LOW
+      xEnconderTick++;                 // then disc is going CW
+    }else{                            // or if B is High
+      xEnconderTick--;                 // then disc is going CCW
+    }
+  }else{                              // If is A falling
+    if(digitalRead(x_ChannelBPin)){   // and B is HIGH
+      xEnconderTick++;                 // then disc is going CW
+    }else{                            // or if B is LOW
+      xEnconderTick--;                 // then disc is going CCW
+    }
+  }
+}
+
+void x_channelBInterruptHandler()
+{
+  if(digitalRead(x_ChannelBPin)){     // If B is rising
+    if(digitalRead(x_ChannelAPin)){   // and A is LOW
+      xEnconderTick++;                 // then disc is going CW
+    }else{                            // or A is HIGH
+      xEnconderTick--;                 // then disc is going CCW
+    }
+  }else{                              // If its falling
+    if(!digitalRead(x_ChannelAPin)){  // Going CW
+      xEnconderTick++;
+    }else{                            // Going CCW
+      xEnconderTick--;
+    }
+  }
+}
+  
+
+void MatlabSendInt(int32_t val){
+  union {
+    int32_t variable;
+    byte temp_array[4];
+  } u;
+  
+  u.variable = val;
+
+  for(int i=0;i<4;i++){
+    Serial.write(u.temp_array[i]);
+  }
+  Serial.print("\n");
+}
+
+void MatlabSendDouble(double val){
+  union {
+    double variable;
+    byte temp_array[8];
+  } u;
+  
+  u.variable = 2536.12523;
+
+  for(int i=0;i<8;i++){
+    Serial.write(u.temp_array[8]);
+  }
+  Serial.print("\n");
+}
+
+
+void printDouble( double val, unsigned int precision){
+   Serial.print (int(val));  //prints the int part
+   Serial.print("."); // print the decimal point
+   unsigned int frac;
+   if(val >= 0)
+       frac = (val - int(val)) * precision;
+   else
+       frac = (int(val)- val ) * precision;
+   Serial.println(frac,DEC) ;
+} 
+
